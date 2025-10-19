c002efb23eb0264b297e33ae7688b453b34e94d8
Ashiepaws <git@ashiepaws.dev>
2025-10-19 17:09:45 +0200

    make dumper use RenderTextures

--- handlers/SpriteDumper.cs
+++ handlers/SpriteDumper.cs
@@ -27,38 +27,84 @@ public static class SpriteDumper
                 if (Plugin.Config.LogSpriteWarnings) Plugin.Logger.LogWarning($"Skipping material {mat.name} with invalid texture size {matTex.width}x{matTex.height}");
                 continue;
             }
-            // TODO: Re-enable this, figuring out sprite loading first
-            Texture2D rawTex = null;
+            if (!matTex.isReadable || matTex is not RenderTexture)
+                matTex = TexUtil.GetReadable(matTex);
+
+            RenderTexture previous = RenderTexture.active;
+            RenderTexture.active = matTex as RenderTexture;
+            GL.PushMatrix();
+            GL.LoadPixelMatrix(0, matTex.width, matTex.height, 0);
 
             string matname = mat.name.Split(' ')[0];
             tk2dSpriteDefinition[] spriteDefinitions = [.. collection.spriteDefinitions.Where(def => def.material == mat)];
-
             foreach (var def in spriteDefinitions)
             {
+                if (string.IsNullOrEmpty(def.name)) continue;
                 if (File.Exists(Path.Combine(DumpPath, collection.name, matname, def.name + ".png")))
                 {
                     if (Plugin.Config.LogSpriteWarnings) Plugin.Logger.LogWarning($"Sprite {def.name} from collection {collection.name}, material {matname} already dumped, skipping.");
                     continue;
                 }
 
-                var spriteTex = TexUtil.GetCutout(rawTex, SpriteUtil.GetSpriteRect(def, rawTex));
-                if (spriteTex == null)
+                Rect spriteRect = SpriteUtil.GetSpriteRect(def, matTex);
+                if(spriteRect.width == 0 || spriteRect.height == 0)
                 {
-                    if (Plugin.Config.LogSpriteWarnings) Plugin.Logger.LogWarning($"Failed to extract sprite {def.name} from material {matname}");
+                    if (Plugin.Config.LogSpriteWarnings) Plugin.Logger.LogWarning($"Skipping sprite {def.name} from collection {collection.name}, material {matname} with invalid size {spriteRect.width}x{spriteRect.height}");
                     continue;
                 }
 
-                if (def.flipped == tk2dSpriteDefinition.FlipMode.Tk2d)
-                    spriteTex = TexUtil.RotateCW(spriteTex);
-                else if (def.flipped == tk2dSpriteDefinition.FlipMode.TPackerCW)
-                    spriteTex = TexUtil.RotateCCW(spriteTex);
+                Texture2D spriteTex = new((int)spriteRect.width, (int)spriteRect.height, TextureFormat.RGBA32, false);
+                spriteTex.ReadPixels(spriteRect, 0, 0);
+                spriteTex.Apply();
 
-                //var png = spriteTex.EncodeToPNG();
-                //IOUtil.EnsureDirectoryExists(Path.Combine(DumpPath, collection.name, matname));
-                //File.WriteAllBytes(Path.Combine(DumpPath, collection.name, matname, def.name + ".png"), png);
+                if (def.flipped == tk2dSpriteDefinition.FlipMode.None)
+                {
+                    var png = spriteTex.EncodeToPNG();
+                    IOUtil.EnsureDirectoryExists(Path.Combine(DumpPath, collection.name, matname));
+                    File.WriteAllBytes(Path.Combine(DumpPath, collection.name, matname, def.name + ".png"), png);
+                    if (Plugin.Config.LogSpriteDumping) Plugin.Logger.LogInfo($"Dumped sprite {def.name} from collection {collection.name}, material {matname}");
+                }
+                else
+                {
+                    RenderTexture rotated = RenderTexture.GetTemporary((int)spriteRect.height, (int)spriteRect.width, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+                    var prev = RenderTexture.active;
+                    RenderTexture.active = rotated;
+                    GL.PushMatrix();
+                    GL.LoadPixelMatrix(0, rotated.width, rotated.height, 0);
 
-                if (Plugin.Config.LogSpriteDumping) Plugin.Logger.LogInfo($"Dumped sprite {def.name} from collection {collection.name}, material {matname}");
+                    Vector2 uBasis, vBasis;
+                    switch (def.flipped)
+                    {
+                        case tk2dSpriteDefinition.FlipMode.Tk2d:
+                            uBasis = Vector2.up; vBasis = Vector2.left;
+                            break;
+                        case tk2dSpriteDefinition.FlipMode.TPackerCW:
+                            uBasis = Vector2.down; vBasis = Vector2.right;
+                            break;
+                        default:
+                            uBasis = Vector2.right; vBasis = Vector2.up;
+                            break;
+                    }
+                    TexUtil.RotateMaterial.SetVector("_Basis", new Vector4(uBasis.x, uBasis.y, vBasis.x, vBasis.y));
+                    Graphics.DrawTextureImpl(new Rect(0, 0, spriteTex.width, spriteTex.height), spriteTex, new Rect(0, 0, 1, 1), 0, 0, 0, 0, Color.white, TexUtil.RotateMaterial, 0);
+
+                    Texture2D finalTex = new((int)spriteRect.height, (int)spriteRect.width, TextureFormat.RGBA32, false);
+                    finalTex.ReadPixels(new Rect(0, 0, rotated.width, rotated.height), 0, 0);
+                    finalTex.Apply();
+
+                    RenderTexture.active = prev;
+                    GL.PopMatrix();
+                    RenderTexture.ReleaseTemporary(rotated);
+
+                    var png = finalTex.EncodeToPNG();
+                    IOUtil.EnsureDirectoryExists(Path.Combine(DumpPath, collection.name, matname));
+                    File.WriteAllBytes(Path.Combine(DumpPath, collection.name, matname, def.name + ".png"), png);
+                    if (Plugin.Config.LogSpriteDumping) Plugin.Logger.LogInfo($"Dumped rotated sprite {def.name} from collection {collection.name}, material {matname}");
+                }
             }
+
+            GL.PopMatrix();
+            RenderTexture.active = previous;
         }
     }
 }
--- util/TexUtil.cs
+++ util/TexUtil.cs
@@ -23,58 +23,6 @@ public static class TexUtil
         return rt;
     }
 
-    public static Texture2D GetCutout(Texture2D source, Rect rect)
-    {
-        if (rect.x < 0 || rect.y < 0 || rect.x + rect.width > source.width || rect.y + rect.height > source.height)
-        {
-            if (Plugin.Config.LogSpriteWarnings) Plugin.Logger.LogWarning($"GetCutout: Rect {rect} is out of bounds for texture size {source.width}x{source.height}");
-            return null;
-        }
-        if (rect.width <= 0 || rect.height <= 0)
-        {
-            if (Plugin.Config.LogSpriteWarnings) Plugin.Logger.LogWarning($"GetCutout: Rect {rect} has non-positive dimensions");
-            return null;
-        }
-
-        Color[] pixels = source.GetPixels((int)rect.x, (int)rect.y, (int)rect.width, (int)rect.height);
-        Texture2D newTex = new((int)rect.width, (int)rect.height, TextureFormat.RGBA32, false);
-        newTex.SetPixels(pixels);
-        newTex.Apply();
-        return newTex;
-    }
-
-    public static Texture2D RotateCW(Texture2D source)
-    {
-        int width = source.width;
-        int height = source.height;
-        Texture2D rotated = new(height, width);
-        for (int i = 0; i < width; i++)
-        {
-            for (int j = 0; j < height; j++)
-            {
-                rotated.SetPixel(height - j - 1, i, source.GetPixel(i, j));
-            }
-        }
-        rotated.Apply();
-        return rotated;
-    }
-
-    public static Texture2D RotateCCW(Texture2D source)
-    {
-        int width = source.width;
-        int height = source.height;
-        Texture2D rotated = new(height, width);
-        for (int i = 0; i < width; i++)
-        {
-            for (int j = 0; j < height; j++)
-            {
-                rotated.SetPixel(j, width - i - 1, source.GetPixel(i, j));
-            }
-        }
-        rotated.Apply();
-        return rotated;
-    }
-
     public static Texture2D LoadFromPNG(string path)
     {
         if (!File.Exists(path))
@@ -91,19 +39,5 @@ public static class TexUtil
             return null;
         }
         return tex;
-    }
-
-    public static Texture2D ResizeTexture(Texture2D source, int newWidth, int newHeight)
-    {
-        RenderTexture rt = RenderTexture.GetTemporary(newWidth, newHeight);
-        Graphics.Blit(source, rt);
-        RenderTexture previous = RenderTexture.active;
-        RenderTexture.active = rt;
-        Texture2D resized = new(newWidth, newHeight);
-        resized.ReadPixels(new Rect(0, 0, newWidth, newHeight), 0, 0);
-        resized.Apply();
-        RenderTexture.active = previous;
-        RenderTexture.ReleaseTemporary(rt);
-        return resized;
     }
 }
